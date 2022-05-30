85bb374e19d831d305d70d3ddd385210eac3ef70
Jinsu Hwang <jhwang39@ucsc.edu>
2022-05-29 22:26:41 -0700

    models clouds

--- models.py
+++ models.py
@@ -22,6 +22,17 @@ db.define_table(
     Field('created_by', default=get_user_email)
 )
 
+db.define_table(
+    'upload',
+    Field('owner', default=get_user_email),
+    Field('file_name'),
+    Field('file_type'),
+    Field('file_date'),
+    Field('file_path'),
+    Field('file_size', 'integer'),
+    Field('confirmed', 'boolean', default=False), # Was the upload to GCS confirmed?
+)
+
 db.post.created_by.readable = db.post.created_by.writable = False
 
 db.define_table(
