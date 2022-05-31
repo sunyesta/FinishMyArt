70408dda409373970984a104bbd836354ebf73f9
Jinsu Hwang <jhwang39@ucsc.edu>
2022-05-30 17:12:50 -0700

    temp changes

--- controllers.py
+++ controllers.py
@@ -34,6 +34,16 @@ from py4web.utils.form import Form, FormStyleBulma
 
 url_signer = URLSigner(session)
 
+BUCKET = '/finishmyart-pictures '
+# GCS keys.  You have to create them for this to work.  See README.md
+GCS_KEY_PATH = os.path.join(APP_FOLDER, 'private/gcs_keys.json')
+with open(GCS_KEY_PATH) as gcs_key_f:
+    GCS_KEYS = json.load(gcs_key_f)
+
+# I create a handle to gcs, to perform the various operations.
+gcs = NQGCS(json_key_path=GCS_KEY_PATH)
+
+
 
 @action('index')
 @action.uses('index.html', db, auth, url_signer)
@@ -106,3 +116,125 @@ def file_upload():
     print("Uploaded", file_name, "of type", file_type)
     print("Content:", uploaded_file.read())
     return "ok"
+
+##----------------------------------------------------------------------##
+@action('file_info')
+@action.uses(url_signer.verify(), db)
+def file_info():
+    row = db(db.upload.owner == get_user_email()).select().first()
+    if row is not None and not row.confirmed:
+        delete_path(row.file_path)
+        row.delete_record()
+        row = {}
+    if row is None:
+        row = {}
+    file_path = row.get('file_path')
+    return dict(
+        file_name=row.get('file_name'),
+        file_type=row.get('file_type'),
+        file_date=row.get('file_date'),
+        file_size=row.get('file_size'),
+        file_path=file_path,
+        download_url=None if file_path is None else gcs_url(GCS_KEYS, file_path),
+        upload_enabled=True,
+        download_enabled=True,
+    )
+
+@action('obtain_gcs', method="POST")
+@action.uses(url_signer.verify(), db)
+def obtain_gcs():
+    """Returns the URL to do download / upload / delete for GCS."""
+    verb = request.json.get("action")
+    if verb == "PUT":
+        mimetype = request.json.get("mimetype", "")
+        file_name = request.json.get("file_name")
+        extension = os.path.splitext(file_name)[1]
+        # Use + and not join for Windows, thanks Blayke Larue
+        file_path = BUCKET + "/" + str(uuid.uuid1()) + extension
+        # Marks that the path may be used to upload a file.
+        mark_possible_upload(file_path)
+        upload_url = gcs_url(GCS_KEYS, file_path, verb='PUT',
+                             content_type=mimetype)
+        return dict(
+            signed_url=upload_url,
+            file_path=file_path
+        )
+    elif verb in ["GET", "DELETE"]:
+        file_path = request.json.get("file_path")
+        if file_path is not None:
+            # We check that the file_path belongs to the user.
+            r = db(db.upload.file_path == file_path).select().first()
+            if r is not None and r.owner == get_user_email():
+                # Yes, we can let the deletion happen.
+                delete_url = gcs_url(GCS_KEYS, file_path, verb='DELETE')
+                return dict(signed_url=delete_url)
+        # Otherwise, we return no URL, so we don't authorize the deletion.
+        return dict(signer_url=None)
+
+@action('notify_upload', method="POST")
+@action.uses(url_signer.verify(), db)
+def notify_upload():
+    """We get the notification that the file has been uploaded."""
+    file_type = request.json.get("file_type")
+    file_name = request.json.get("file_name")
+    file_path = request.json.get("file_path")
+    file_size = request.json.get("file_size")
+    # Deletes any previous file.
+    rows = db(db.upload.owner == get_user_email()).select()
+    for r in rows:
+        if r.file_path != file_path:
+            delete_path(r.file_path)
+    # Marks the upload as confirmed.
+    d = datetime.datetime.utcnow()
+    db.upload.update_or_insert(
+        ((db.upload.owner == get_user_email()) &
+         (db.upload.file_path == file_path)),
+        owner=get_user_email(),
+        file_path=file_path,
+        file_name=file_name,
+        file_type=file_type,
+        file_date=d,
+        file_size=file_size,
+        confirmed=True,
+    )
+    # Returns the file information.
+    return dict(
+        download_url=gcs_url(GCS_KEYS, file_path, verb='GET'),
+        file_date=d,
+    )
+
+@action('notify_delete', method="POST")
+@action.uses(url_signer.verify(), db)
+def notify_delete():
+    file_path = request.json.get("file_path")
+    # We check that the owner matches to prevent DDOS.
+    db((db.upload.owner == get_user_email()) &
+       (db.upload.file_path == file_path)).delete()
+    return dict()
+
+def delete_path(file_path):
+    """Deletes a file given the path, without giving error if the file
+    is missing."""
+    try:
+        bucket, id = os.path.split(file_path)
+        gcs.delete(bucket[1:], id)
+    except:
+        # Ignores errors due to missing file.
+        pass
+
+def delete_previous_uploads():
+    """Deletes all previous uploads for a user, to be ready to upload a new file."""
+    previous = db(db.upload.owner == get_user_email()).select()
+    for p in previous:
+        # There should be only one, but let's delete them all.
+        delete_path(p.file_path)
+    db(db.upload.owner == get_user_email()).delete()
+
+def mark_possible_upload(file_path):
+    """Marks that a file might be uploaded next."""
+    delete_previous_uploads()
+    db.upload.insert(
+        owner=get_user_email(),
+        file_path=file_path,
+        confirmed=False,
+    )
